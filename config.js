360f18256a6eaf0d3af056681662a7a1aa11ef9f
Nishchay Kala <nishchay@iitj.ac.in>
2013-06-05 04:34:03 -0600

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,6 @@ var config = {
             { text: 'Documentation',
               link: 'https://github.com/sankha93/grite/wiki/Documentation'} ],
   posts: [ '5711471',
-           '5711492',
            '5712982'],
   atOnce: 5,
   footerText: "Grite has been made with love by Sankha. Follow him on Twitter at <a href=\"https://twitter.com/sankha93\">@sankha93</a>."
